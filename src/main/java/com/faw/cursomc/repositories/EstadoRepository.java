76f672b4d8ae45d2b0cbbf52afcfc1929cdf0cbb
Fernando Alexandre Wahl <wahlfernando@yahoo.com.br>
2022-01-05 22:34:15 -0300

    Estado e Cidade

---
+++ src/main/java/com/faw/cursomc/repositories/EstadoRepository.java
@@ -0,0 +1,13 @@
+package com.faw.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.faw.cursomc.domain.Estado;
+
+@Repository
+public interface EstadoRepository extends JpaRepository<Estado, Integer> {
+
+
+
+}
