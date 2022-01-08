4cd995531a87eca73b6d3b14ad07a2d97191564a
Fernando Alexandre Wahl <wahlfernando@yahoo.com.br>
2022-01-08 14:46:06 -0300

    Cliente, Tipo cliente, Telefones e Enderecos

--- src/main/java/com/faw/cursomc/CursomcApplication.java
+++ src/main/java/com/faw/cursomc/CursomcApplication.java
@@ -9,10 +9,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.faw.cursomc.domain.Categoria;
 import com.faw.cursomc.domain.Cidade;
+import com.faw.cursomc.domain.Cliente;
+import com.faw.cursomc.domain.Endereco;
 import com.faw.cursomc.domain.Estado;
 import com.faw.cursomc.domain.Produto;
+import com.faw.cursomc.domain.enuns.TipoCliente;
 import com.faw.cursomc.repositories.CategoriaRepository;
 import com.faw.cursomc.repositories.CidadeRepository;
+import com.faw.cursomc.repositories.ClienteRepository;
+import com.faw.cursomc.repositories.EnderecoRepository;
 import com.faw.cursomc.repositories.EstadoRepository;
 import com.faw.cursomc.repositories.ProdutoRepository;
 
@@ -30,6 +35,12 @@ public class CursomcApplication implements CommandLineRunner{
 	
 	@Autowired
 	private CidadeRepository cidadeRepository;
+	
+	@Autowired
+	private ClienteRepository clienteRepository;
+	
+	@Autowired
+	private EnderecoRepository enderecoRepository;
 	
 	
 	public static void main(String[] args) {
@@ -67,6 +78,21 @@ public class CursomcApplication implements CommandLineRunner{
 		
 		estadoRepository.saveAll(Arrays.asList(est1, est2));
 		cidadeRepository.saveAll(Arrays.asList(c1, c2, c3));
+		
+		
+		Cliente cli1 = new Cliente(null, "Maria Silva", "asdasdasd@asdasd.com.br", "36378912377", TipoCliente.PESSOAFISICA);
+		
+		cli1.getTelefones().addAll(Arrays.asList("27396587", "93838393"));
+		
+		Endereco e1 = new Endereco(null, "Rua Flores", "300", "Apto 303", "Jardim", "38220834", cli1, c1);
+		Endereco e2 = new Endereco(null, "Av Matos", "105", "Sala 800", "Centro", "38777012", cli1, c2);
+		
+		cli1.getEnderecos().addAll(Arrays.asList(e1, e2));
+		
+		clienteRepository.saveAll(Arrays.asList(cli1));
+		enderecoRepository.saveAll(Arrays.asList(e1, e2));
+		
+		
 		
 		
 	}
---
+++ src/main/java/com/faw/cursomc/domain/enuns/TipoCliente.java
@@ -0,0 +1,42 @@
+package com.faw.cursomc.domain.enuns;
+
+public enum TipoCliente {
+	
+	PESSOAFISICA(1, "Pessoa Fisica"),
+	PESSOAJURIDICA(2, "Pessoa Jurídica");
+
+	
+	private int cod;
+	private String descricao;
+	
+	private TipoCliente(int cod, String descricao) {
+		this.cod = cod;
+		this.descricao = descricao;
+	}
+	
+	public int getCod() {
+		return cod;
+	}
+	
+	public String getDescricao() {
+		return descricao;
+	}
+	
+	public static TipoCliente toEnum(Integer cod) {
+		
+		if(cod == null) {
+			return null;
+		}
+		
+		for (TipoCliente x : TipoCliente.values()) {
+			if (cod.equals(x.getCod())) {
+				return x;
+			}
+		}
+		
+		throw new IllegalArgumentException("Id inválido: " + cod);
+		
+	}
+	
+	
+}
